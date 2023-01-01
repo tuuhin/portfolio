7a13a7a3f94b9ab0d27a21f99a2934a963cfb4c7
tuuhin <tuhinbhowmick2513@gmail.com>
2023-01-01 23:48:21 +0530

    Prepared the footer and the navbar Not 100% correct but kind a similar type of footer is made For navbar it need to be modified

---
+++ src/components/NavBar/NavBar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { slide } from "svelte/transition";
+
+  import NavItems from "./NavItems.svelte";
+  import NavButton from "./NavButton.svelte";
+
+  let isFocused = false;
+  const onClick = () => (isFocused = !isFocused);
+</script>
+
+<nav class="bg-slate-800">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="flex h-16 items-center justify-between">
+      <div class="-mr-2 flex md:hidden">
+        <NavButton {onClick} />
+      </div>
+      <div class="flex items-center">
+        <div class="flex-shrink-0">
+          <img
+            class="h-8 w-8"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+            alt="Your Company"
+          />
+        </div>
+        <div class="hidden md:block">
+          <NavItems isflex={true} />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="md:hidden" id="mobile-menu">
+    {#if isFocused}
+      <div transition:slide class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+        <NavItems />
+      </div>
+    {/if}
+  </div>
+</nav>
---
+++ src/components/NavBar/NavItems.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let isflex: boolean = false;
+</script>
+
+<div class={isflex ? "flex ml-10 items-baseline space-x-4" : ""}>
+  <a
+    href="/#"
+    class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+    aria-current="page">Dashboard</a
+  >
+
+  <a
+    href="/#Team"
+    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+    >Team</a
+  >
+
+  <a
+    href="/#Projects"
+    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+    >Projects</a
+  >
+
+  <a
+    href="/#Calender"
+    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+    >Calendar</a
+  >
+
+  <a
+    href="/#Reports"
+    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+    >Reports</a
+  >
+</div>
