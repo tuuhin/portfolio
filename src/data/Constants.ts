485bdd31182f7df52ecabbeeb77cfac361b77153
tuuhin <tuhinbhowmick2513@gmail.com>
2024-10-15 12:37:23 +0530

    New font and removed image in about Used the new font at heading in most of the lead components.

--- src/data/Constants.ts
+++ src/data/Constants.ts
@@ -8,3 +8,8 @@ export const HOME_DESC =
 export const OG_IMAGE = "/images/og_image.svg";
 
 export const WEBSITE_URL = "https://tuhinbhowmick.in";
+
+export const HERO_TITLE = "Tuhin Bhowmick";
+
+export const HERO_DESC =
+  "Android developer with a strong foundation in mobile app development. Possessing two years of hands-on experience in building robust and user-centric applications.";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				"playfair": ['"Playfair Display"'],
-				"poppins": ["Poppins"],
-				"inconsolata": ["Inconsolata"]
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"'],
+        poppins: ["Poppins"],
+        inconsolata: ["Inconsolata"],
+        monument: ["MonumentExtended"],
+      },
+    },
+  },
+  plugins: [],
+};
